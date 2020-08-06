f9df0f9b7c417ed39348a9690d97b91113cb1e9b
jlandesman <jlandesman@users.noreply.github.com>
2020-08-06 16:20:19 -0400

    Update daily_update.sh

--- daily_update.sh
+++ daily_update.sh
@@ -7,7 +7,7 @@ git_export_all_file_versions by-age.csv /home/ubuntu/nyc_covid_data_disaggregate
 git_export_all_file_versions data-by-modzcta.csv /home/ubuntu/nyc_covid_data_disaggregated/data/by_zip
 git_export_all_file_versions data-by-boro.csv /home/ubuntu/nyc_covid_data_disaggregated/data/by_boro
 
-cd /tmp/nyc_covid_data
+cd /home/ubuntu/nyc_covid_data_disaggregated
 git add .
 git commit -m "Uploading latest data"
 git push origin master
