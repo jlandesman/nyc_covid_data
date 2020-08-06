141c89e6341ac2613abe487cbeb10cb67268d062
jlandesman <jlandesman@users.noreply.github.com>
2020-08-06 16:26:11 -0400

    Update daily_update.sh

--- daily_update.sh
+++ daily_update.sh
@@ -5,7 +5,7 @@ git pull
 
 git_export_all_file_versions by-age.csv /home/ubuntu/nyc_covid_data/data/by_age
 git_export_all_file_versions data-by-modzcta.csv /home/ubuntu/nyc_covid_data/data/by_zip
-git_export_all_file_versions data-by-boro.csv /home/ubuntu/nyc_covid_data/data/by_boro
+git_export_all_file_versions by-boro.csv /home/ubuntu/nyc_covid_data/data/by_boro
 
 cd /home/ubuntu/nyc_covid_data
 git add .
