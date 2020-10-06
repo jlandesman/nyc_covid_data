b24a7024e65c1e2303ad81886371198b3ba6c5b5
Ubuntu <ubuntu@ip-172-31-37-194.ec2.internal>
2020-10-06 17:27:36 +0000

    Uploading latest data

--- daily_update.sh
+++ daily_update.sh
@@ -6,6 +6,7 @@ git pull
 git_export_all_file_versions by-age.csv /home/ubuntu/nyc_covid_data/data/by_age
 git_export_all_file_versions data-by-modzcta.csv /home/ubuntu/nyc_covid_data/data/by_zip
 git_export_all_file_versions by-boro.csv /home/ubuntu/nyc_covid_data/data/by_boro
+git_export_all_file_versions recent/recent-4-week-by-modzcta.csv /home/ubuntu/nyc_covid_data/data/recent
 
 cd /home/ubuntu/nyc_covid_data
 git add .
